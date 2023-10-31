0a960fa8f88bb7430af9c702acb8d0bf3c4478b8
Nirudium <dude@whatever.com>
2023-10-31 19:49:59 +0000

    Neovim changes with a bunch of immaturity.

--- home-manager/cli/neovim.nix
+++ home-manager/cli/neovim.nix
@@ -1,7 +1,5 @@
 {
-  pkgs,
   inputs,
-  lib,
   ...
 }: {
   imports = [ inputs.nixvim.homeManagerModules.nixvim ];
@@ -10,21 +8,54 @@
     defaultEditor = true;
 
     colorschemes.oxocarbon.enable = true;
+    
     plugins = {
       lightline.enable = true;
       chadtree.enable = true;
       treesitter.enable = true;
-      nvim_lsp.enable = true; 
+      lsp = {
+	enable = true;
+	servers = {
+	  nil_ls.enable = true; 
+	};
+      };
       
 
       #cmp
-      nvim-cmp.enable = true;
+      nvim-cmp = {
+	sources = [
+	  { name = "nvim_lsp"; }
+	  { name = "luasnip"; } #For luasnip users.
+	  { name = "path"; }
+	  { name = "buffer"; }
+	];
+      };
       cmp-treesitter.enable = true;
       cmp-vim-lsp.enable = true; 
       cmp-buffer.enable = true;
       cmp-path.enable = true;
-    };
 
+      startify = {
+	enable = true;
+	customHeader = [
+  ''⠀ ▄▄▄▄    █    ██  ██ ▄█▀ ██ ▄█▀ ▄▄▄       ██ ▄█▀▓█████ ''
+  ''⠀▓█████▄  ██  ▓██▒ ██▄█▒  ██▄█▒ ▒████▄     ██▄█▒ ▓█   ▀ ''
+  ''⠀▒██▒ ▄██▓██  ▒██░▓███▄░ ▓███▄░ ▒██  ▀█▄  ▓███▄░ ▒███   ''
+  ''⠀▒██░█▀  ▓▓█  ░██░▓██ █▄ ▓██ █▄ ░██▄▄▄▄██ ▓██ █▄ ▒▓█  ▄ ''
+  ''⠀░▓█  ▀█▓▒▒█████▓ ▒██▒ █▄▒██▒ █▄ ▓█   ▓██▒▒██▒ █▄░▒████▒''
+  ''⠀░▒▓███▀▒░▒▓▒ ▒ ▒ ▒ ▒▒ ▓▒▒ ▒▒ ▓▒ ▒▒   ▓▒█░▒ ▒▒ ▓▒░░ ▒░ ░''
+  ''⠀▒░▒   ░ ░░▒░ ░ ░ ░ ░▒ ▒░░ ░▒ ▒░  ▒   ▒▒ ░░ ░▒ ▒░ ░ ░  ░''
+  ''⠀ ░    ░  ░░░ ░ ░ ░ ░░ ░ ░ ░░ ░   ░   ▒   ░ ░░ ░    ░   ''
+  ''⠀ ░         ░     ░  ░   ░  ░         ░  ░░  ░      ░  ░''
+  ''⠀      ░                                                ''
+	];
+	bookmarks = [
+	  "/home/noor/Sysflake/"
+	];
+    };
+  };
+
+    # End of Plugins 
     options = {
       number = true; 
       shiftwidth = 2;
