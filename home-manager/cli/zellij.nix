b5162b381e0f48c4b7640953dc8f06ecb68cb3f1
Nirudium <dude@whatever.com>
2023-11-15 11:05:17 -0800

    Made some changes to zellij to open nvim as default editor
    Yayyyyy

--- home-manager/cli/zellij.nix
+++ home-manager/cli/zellij.nix
@@ -2,19 +2,22 @@
   programs.zellij = {
     enable = true;
     enableFishIntegration = true;
-    settings.theme = "tokyo-night-dark";
-    settings.themes.tokyo-night-dark = {
-      fg = "#A9B1D6";
-      bg = "#1A1B26";
-      black = "#383E5A";
-      red = "#F93357";
-      green = "#9ECE6A";
-      yellow = "#E0AF68";
-      blue = "#7AA2F7";
-      magenta = "#BB9AF7";
-      cyan = "#2AC3DE";
-      white = "#C0CAF5";
-      orange = "#FF9E64";
+    settings = {
+      scrollback_editor = "${pkgs.neovim}/bin/nvim";
+      theme = "tokyo-night-dark";
+      themes.tokyo-night-dark = {
+        fg = "#A9B1D6";
+        bg = "#1A1B26";
+        black = "#383E5A";
+        red = "#F93357";
+        green = "#9ECE6A";
+        yellow = "#E0AF68";
+        blue = "#7AA2F7";
+        magenta = "#BB9AF7";
+        cyan = "#2AC3DE";
+        white = "#C0CAF5";
+        orange = "#FF9E64";
+      };
     };
   };
 }
