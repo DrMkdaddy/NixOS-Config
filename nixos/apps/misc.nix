6ef90b923ffc982d204721499fc8649f051ad82e
Noor Mkdad <alph4nir@riseup.net>
2024-04-10 23:57:23 -0700

    Trying to set this up.

--- nixos/apps/misc.nix
+++ nixos/apps/misc.nix
@@ -1,9 +1,13 @@
-{pkgs, ...}:
 {
+  pkgs,
+  inputs,
+  ...
+}: {
   users.users.noor.packages = with pkgs; [
     vulkan-tools
     jre8
     temurin-jre-bin-17
     geekbench
+    inputs.ragenix.packages.x86_64-linux.default
   ];
 }
