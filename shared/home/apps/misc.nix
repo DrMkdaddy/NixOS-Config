906049795201a18620384e9604cafcee822280a4
Noor Mkdad <alph4nir@riseup.net>
2024-05-18 21:47:37 -0700

    Added vesktop

--- shared/home/apps/misc.nix
+++ shared/home/apps/misc.nix
@@ -1,17 +1,13 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     obsidian
-    (discord.override {
-      withOpenASAR = true;
-      withVencord = true;
-    })
     gamescope
     thunderbird
     element-desktop
     prismlauncher
     mpv
-    armcord
     chromium
     nicotine-plus
+    vesktop
   ];
 }
