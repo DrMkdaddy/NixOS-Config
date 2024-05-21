503945324b02cfb081254d9f6f21bc533ac68e46
Noor Mkdad <alph4nir@riseup.net>
2024-05-20 21:56:27 -0700

    Added nvidia for Tdarr to do transcoding

--- nasr/configuration.nix
+++ nasr/configuration.nix
@@ -9,46 +9,24 @@
     driSupport32Bit = true;
   };
 
-  # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    # Modesetting is required.
     modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
   boot.initrd.kernelModules = ["nvidia"];
   environment = {
     systemPackages = with pkgs; [
-      dive # look into docker image layers
-      podman-tui # status of containers in the terminal
-      docker-compose # start group of containers for dev
-      #podman-compose # start group of containers for dev
+      dive
+      podman-tui
+      docker-compose
+      #podman-compose
       vesktop
     ];
     sessionVariables = {
@@ -67,4 +45,6 @@
       PasswordAuthentication = false;
     };
   };
+
+  virtualisation.containers.cdi.dynamic.nvidia.enable = true;
 }
