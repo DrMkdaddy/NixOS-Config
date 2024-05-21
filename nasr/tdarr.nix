00ab6d2b41e4ee6d0ca56cf0511adbb1874e2428
Noor Mkdad <alph4nir@riseup.net>
2024-05-20 23:13:46 -0700

    updated tdarr

--- nasr/tdarr.nix
+++ nasr/tdarr.nix
@@ -1,5 +1,6 @@
 _: {
   virtualisation.oci-containers.containers.tdarr = {
+    autoStart = true;
     image = "ghcr.io/haveagitgat/tdarr";
     volumes = [
       "/home/noor/docker/tdarr/server:/app/server"
@@ -10,11 +11,11 @@ _: {
     ];
     environment = {
       serverIP = "0.0.0.0";
-      serverPort = 8266;
-      webUIPort = 8266;
-      internalNode = true;
-      inContainer = true;
-      ffmpegVersion = 6;
+      serverPort = "8266";
+      webUIPort = "8265";
+      internalNode = "true";
+      inContainer = "true";
+      ffmpegVersion = "6";
       nodeName = "InternalNode";
       TZ = "America/Los_Angeles";
       NVIDIA_DRIVER_CAPABILITIES = "all";
@@ -23,7 +24,6 @@ _: {
     ports = ["8265:8265" "8266:8266"];
     extraOptions = [
       "-ti"
-      "--network bridge"
       "-gpus=all"
       "--device=/dev/dri:/dev/dri"
       "--log-opt max-size=10m"
