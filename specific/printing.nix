9267af40a978d1e450302e497f44514b68a82f58
Noor Mkdad <alph4nir@riseup.net>
2024-05-26 11:54:11 -0700

    Bad printing protection

--- specific/printing.nix
+++ specific/printing.nix
@@ -1,3 +1,11 @@
 {pkgs, ...}: {
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    listenAddresses = ["*:631"];
+    allowFrom = ["all"];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+    drivers = [pkgs.brlaser];
+  };
 }
